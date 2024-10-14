a410ee9cc2676eb83dd082075b2a87d8a17bfffe
Ashu <Ashu@users.noreply.github.com>
2024-10-14 15:10:07 +0530

    - Created Country model & updated Address model - Updated admin to include Address and Country

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,27 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+    
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.city} - {self.postal_code}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
     
 class Author(models.Model):
     first_name = models.CharField(max_length=1000)
@@ -27,6 +44,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
+    published_country = models.ManyToManyField(Country, null=False)
 
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
